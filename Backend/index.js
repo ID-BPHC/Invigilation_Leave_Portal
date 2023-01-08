9aeead60d73e58c435a14053e2d14fae92f8a604
Nischay Mamidi <NischayPro@gmail.com>
2023-01-08 14:17:01 +0530

    Change PORT to 5004 for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5004;
 
 // submit endpoint when user fills the form for leave
 app.post("/submit", async (req, res) => {
