917d5b7867eadf5e0d855397a1bc3bfb99b6c175
soumitradev <soumitra.shewale.dev@gmail.com>
2023-01-10 16:13:00 +0530

    Make changes to config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-REACT_APP_BASEURL : 'http://localhost:3001',
-REACT_APP_APIURL : 'http://localhost:5001',
-REACT_APP_FIREBASE_API_KEY : 'AIzaSyD_vTZ4BbXeH0r5BsWkO0ZJEcUnyidBZrc',
-REACT_APP_APP_ID : '1:907300732555:web:f4c0ca6ee94bf77c435e8e'
-}
+  REACT_APP_BASEURL: "http://localhost:3008",
+  REACT_APP_APIURL: "http://localhost:5004",
+  REACT_APP_FIREBASE_API_KEY: "AIzaSyD_vTZ4BbXeH0r5BsWkO0ZJEcUnyidBZrc",
+  REACT_APP_APP_ID: "1:907300732555:web:f4c0ca6ee94bf77c435e8e",
+};
