216f5d514a36dec1eaac09015bfedcd1cb2414f9
Pavas Garg <pavasgarg2003@gmail.com>
2023-02-27 15:58:33 +0530

    Update HOD_Data.js

--- Client/src/Admin/HOD_Data.js
+++ Client/src/Admin/HOD_Data.js
@@ -32,7 +32,7 @@ export const HOD_Data = [
   {
     value: "Economics and Finance",
     text: "Economics and Finance",
-    hod: ["dushyant@hyderabad.bits-pilani.ac.in"],
+    hod: ["f20212587@hyderabad.bits-pilani.ac.in"],
   },
   {
     value: "Electrical and Electronics Engineering",
