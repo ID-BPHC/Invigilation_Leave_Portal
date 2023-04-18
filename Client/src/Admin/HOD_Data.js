9c6ee0ad8b48e768a246df0d99055fe25e88c810
Pavas Garg <pavasgarg2003@gmail.com>
2023-04-18 13:43:44 +0530

    updated bio hod email

--- Client/src/Admin/HOD_Data.js
+++ Client/src/Admin/HOD_Data.js
@@ -7,7 +7,7 @@ export const HOD_Data = [
   {
     value: "Biological Sciences",
     text: "Biological Sciences",
-    hod: ["hod.biology@hyderabad.bits-pilani.ac.in"],
+    hod: ["sangan@hyderabad.bits-pilani.ac.in"],
   },
   {
     value: "Chemical Engineering",
